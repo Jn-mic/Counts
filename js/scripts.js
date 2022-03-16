a7d1538d18736ac203698c286345de6a7161a79c
jack <jackotienokey@gmail.com>
2022-03-17 01:03:22 +0300

    js behaviour setting

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,21 @@
 //set count value//
 let count = 0;
 
-const value= document.querySelector('#value');
-const btns= document.querySelectorAll('.btn')
+const value= document.querySelector('.value');
+const btns= document.querySelectorAll('.btn');
 
 btns.forEach(function(){
-    
+    btn.addEventLister('click',function(e){
+        const style = e.currentTarget.classList
+        if(style.contains('decrease')){
+            count --;
+        }
+        else if(style.contains('increase')){
+            count ++;
+        }
+        else{
+            count = 0;
+        }
+        value.textContent = count;
+    })
 })
