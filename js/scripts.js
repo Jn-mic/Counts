2de0daf5dfb0eab1ac4dbbacc9097fe77fb17bf3
jack <jackotienokey@gmail.com>
2022-03-17 20:49:16 +0300

    update'

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
 //set count value//
 let count = 0;
 
-const value= document.querySelector('.value');
+const value= document.querySelector('#value');
 const btns= document.querySelectorAll('.btn');
 
 btns.forEach(function(){
     btn.addEventLister('click',function(e){
-        const style = e.currentTarget.classList
-        if(style.contains('decrease')){
+        const styles = e.currentTarget.classList;
+        if(styles.contains('decrease')){
             count --;
         }
-        else if(style.contains('increase')){
+        else if(styles.contains('increase')){
             count ++;
         }
         else{
             count = 0;
         }
         value.textContent = count;
-    })
-})
+    });
+});
